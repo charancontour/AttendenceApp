5b153dc66d79a2ce1d8cdbacc5e811ae60a7884e
charan <charan@contourgroupinc.com>
2018-02-08 11:35:54 +0530

    added dates to MonthlySalary Models

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -7,22 +7,10 @@ use App\MonthlySalary;
 use App\Employee;
 class ReportController extends Controller
 {
-    //
-    public function index()
-    {
-        
-    		$monthlysalaries		=		MonthlySalary::all();
-
-    		//dd($monthlysalaries);
-    		$employees=Employee::all();
-
-    		//dd($employees);
-    		
-            
-    		return view('report.index')->with('monthlysalaries',$monthlysalaries)->with('employees',$employees);
-
-
-
-    }
+  public function index()
+  {
+		$monthly_salaries		=		MonthlySalary::all();
+    return view('report.index')->with('monthly_salaries',$monthly_salaries);
+  }
 
 }
--- app/MonthlySalary.php
+++ app/MonthlySalary.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MonthlySalary extends Model
 {
-    //
+    protected $dates = ['month'];
+
+    public function employee()
+    {
+      return $this->belongsTo('App\Employee');
+    }
 }
